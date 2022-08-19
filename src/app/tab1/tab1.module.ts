8f6d0fca0e23c3699110fb32a8f215c367f5b9db
yousef <gheibiyousef2000@gmail.com>
2022-08-19 17:43:38 +0430

    implement add to storage

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
@@ -11,7 +12,8 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    IonicStorageModule.forRoot()
   ],
   declarations: [Tab1Page]
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
-
+import { Storage } from '@ionic/storage-angular';
+interface Budget {
+  val: number;
+  date: string;
+  description: string;
+  type: string;
+};
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+  val: number;
+  description: string;
+  items: Array<Budget> = [];
+  constructor(private storage: Storage) {
+    this.val = 1000;
+    storage.create();
+  }
 
-  constructor() {}
+  ionViewDidEnter(){
+    this.storage.get('items').then((val)=>{
+      if(val){
+        this.items = val;
+      }
+    });
+  }
 
+  register(type: string){
+    let ftype: string;
+    if(type === 'income'){
+      ftype = 'درآمد';
+    } else{
+      ftype = 'هزینه';
+    }
+    this.items.push({
+      val:this.val,
+      date:new Date().toLocaleDateString('fa-IR'),
+      description: this.description,
+      type: ftype
+    });
+    this.storage.set('items', this.items);
+  }
+
+  checkValue(type: string){
+    if(type === 'mins'){
+      this.val = this.val - 1000;
+    }
+    else if(type === 'plus'){
+      this.val = this.val + 1000;
+    }
+  }
 }
